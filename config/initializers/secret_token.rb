eeb5fb002b4ebe03ef6c63a40177ee721875feca
vinay <vinay@vertisinfotech.com>
2013-02-12 14:40:23 +0530

    created initial skelton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = '99509225ba7aa68adc99c6469a5700207c12205506285536e7a033e38ad23d9954e7b224feb5110407dd823be30fe651a0afa577c9750a72d5b5d6fa2a474456'
