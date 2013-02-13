b31b74d468d8a07f272e73d29815624b0efae459
vinay <vinay@vertisinfotech.com>
2013-02-13 18:16:08 +0530

    committing with error in _form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Blogger::Application.routes.draw do
   resources :articles
+  resources :comments
 
-  #root to: 'articles#index'
+  root to: 'articles#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
