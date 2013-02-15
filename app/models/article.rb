bcfdfab9fc343a145efbc53740333d0f530d39da
vinay <vinay@vertisinfotech.com>
2013-02-15 19:21:39 +0530

    finished tagging part

--- app/models/article.rb
+++ app/models/article.rb
@@ -8,13 +8,14 @@ class Article < ActiveRecord::Base
 	    return self.tags.join(", ")
 	  end
 
-	  def tag_list=(tags_string)
-        tag_names = tags_string.split(",").collect{|s| s.strip.downcase}
-        tag_names.each do |tag_name|
-        tag = Tag.find_or_create_by_name(tag_name)
-            tagging = self.taggings.new
-            tagging.tag_id = tag.id
-        end
+	def tag_list=(tags_string)
+    self.taggings.destroy_all
+    tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
+    tag_names.each do |tag_name|
+    tag = Tag.find_or_create_by_name(tag_name)
+    tagging = self.taggings.new
+    tagging.tag_id = tag.id
+  end
 end
 
 end
